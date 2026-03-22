0ca8676413b816ab371f89df6076995a013c88c1
Melvin Loos <loos@glr.nl>
2026-03-22 12:16:55 +0100

    feat(branding): add custom favicon and update build config to use static assets

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  publicDir: 'static', // Define a new directory for static assets to avoid conflicting with outDir
+  build: {
+    outDir: 'public',
+    emptyOutDir: true // Maakt de map leeg voor elke build, voorkomt conflicten
+  }
 })
