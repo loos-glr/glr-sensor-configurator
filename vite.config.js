9eead7597a9e0923de1f984e3c1b1d6fce41cbef
Melvin Loos <loos@glr.nl>
2026-03-22 12:51:47 +0100

    fix(build): restored default build config

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  publicDir: 'static', // Define a new directory for static assets to avoid conflicting with outDir
   build: {
-    outDir: 'public',
+    outDir: 'dist',
     emptyOutDir: true // Maakt de map leeg voor elke build, voorkomt conflicten
   },
   rollupOptions: {
